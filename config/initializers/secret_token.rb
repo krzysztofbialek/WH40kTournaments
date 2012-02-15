1bf50dcb1722a9983babe829e4783c903323194d
krzysztofbialek <zlo@o2.pl>
2012-02-15 01:01:16 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dmp40k::Application.config.secret_token = 'f1da59089c97782ae78254c0e881aa9f976fa8eee2674f876c0d6d97630d93c853c74fef4f49ed6879978612501e1ee91562308e2685299649058059d223e6b7'
