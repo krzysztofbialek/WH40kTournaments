979e5b415eb95c52f76c90ed1368aba4565540e7
krzysztofbialek <zlo@o2.pl>
2012-09-09 21:40:21 +0200

    add armies

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,10 @@
 class Player < ActiveRecord::Base
   require 'csv'
 
-  Armies = ['Grey Knights', 'Eldars', 'Chaos Space Marines', 'Space Wolves']
+  Armies = ['Grey Knights', 'Eldars', 'Chaos Space Marines', 'Space Wolves',
+             'Imperial Guard', 'Chaos Demons', 'Blood Angels', 'Dark Angels',
+             'Orks', 'Dark Eldars', 'Black Templars', 'Sister of Battle',
+             'Tyranids', 'Necrons', 'Tau']
 
   has_many :tournament_registrations, :dependent => :nullify
 
