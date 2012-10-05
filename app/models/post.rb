25084efaf44a1fd1e763f9fd27d66f431baf3656
krzysztofbialek <zlo@o2.pl>
2012-10-05 15:07:16 +0200

    slugify posts and add blank to armies

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ActiveRecord::Base
+  extend FriendlyId
 
   belongs_to :user
   belongs_to :tournament
 
   default_scope :order => 'created_at DESC'
+  friendly_id :name, use: [:slugged, :scoped], scope: :tournament
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121005123421) do
+ActiveRecord::Schema.define(:version => 20121005130150) do
 
   create_table "hostel_bookings", :force => true do |t|
     t.string   "first_name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(:version => 20121005123421) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "tournament_id"
+    t.string   "slug"
   end
 
   create_table "team_registrations", :force => true do |t|
