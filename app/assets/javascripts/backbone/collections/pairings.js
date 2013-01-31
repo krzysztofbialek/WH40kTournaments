7ca562fef94e45666e2f4b1e313a6d10af5752c7
krzysztofbialek <zlo@o2.pl>
2013-01-31 15:12:40 +0100

    updates to pairings and round

--- app/assets/javascripts/backbone/collections/pairings.js
+++ app/assets/javascripts/backbone/collections/pairings.js
@@ -14,13 +14,22 @@ Pairings = Backbone.Collection.extend({
   generateNewRound: function(){
     this.url = '/tournaments/' + Dmp40k.getTournamentId() + '/generate_pairings';
     this.fetch({
-      success: function(response, xhr){
-        console.log('cool')
-      },
+      success: _.bind(function(response, xhr){
+        this.updateRound();
+      }, this),
       error: function(collection, error){
         alert(JSON.parse(error.responseText).msg);
-      },
+      }
     });
+  },
+
+  updateRound: function(){
+    console.log('update')
+    text = $('.round').text();
+    n = parseInt(text.match(/\d+/));
+    text = text.replace(/\d+/, n + 1);
+    $('.round').text(text);
+    console.log(text);
   },
 
 });
