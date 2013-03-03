f3cb08e5efd7a069c21024e86993956af8769008
Łukasz Pełszyński <lukaszp@lavabit.com>
2013-03-04 00:16:04 +0100

    Pairings return all the pairings /dirty sql/

--- app/models/player.rb
+++ app/models/player.rb
@@ -9,6 +9,13 @@ class Player < ActiveRecord::Base
   has_many :tournament_registrations, :dependent => :nullify
   has_many :player1_pairings, :class_name => 'TournamentPairing', :foreign_key => 'player1_id'
   has_many :player2_pairings, :class_name => 'TournamentPairing', :foreign_key => 'player2_id'
+  has_many :pairings, :class_name => 'TournamentPairing', :finder_sql => Proc.new {
+    %Q{
+      SELECT DISTINCT *
+      FROM tournament_pairings AS tp
+      WHERE tp.player1_id = #{id} OR tp.player2_id = #{id}
+    }
+}
   
   def pairings
      player1_pairings + player2_pairings
