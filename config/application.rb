c0a10a2a200f43468c02b9ac6087b17ffd87a297
krzysztofbialek <zlo@o2.pl>
2013-04-22 11:45:08 +0200

    update config

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,8 @@ module Dmp40k
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    config.assets.precompie += %w( play.js )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
