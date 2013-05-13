7b27e2d92d9084354add583e497834e143d513c0
krzysztofbialek <zlo@o2.pl>
2013-05-13 14:08:33 +0200

    allow only active users to log in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130511205915) do
+ActiveRecord::Schema.define(:version => 20130513120806) do
 
   create_table "hostel_bookings", :force => true do |t|
     t.string   "first_name"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(:version => 20130511205915) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin"
+    t.boolean  "active"
   end
 
 end
