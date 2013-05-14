6484d2265cf9f6f617e87c390e08c60d21751511
krzysztofbialek <zlo@o2.pl>
2013-05-14 12:39:11 +0200

    assets precompile

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module Dmp40k
     # Enable the asset pipeline
     config.assets.enabled = true
     
-    config.assets.precompile += %w( play.js )
+    config.assets.precompile += %w( play.js jquery-1.7.js )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
