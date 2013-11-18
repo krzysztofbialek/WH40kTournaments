129f40a958a1fc2288f4db2870825d4ccc746cd1
krzysztofbialek <zlo@o2.pl>
2013-11-18 20:15:50 +0100

    add join model for players and team_registrations

--- app/models/team_registration.rb
+++ app/models/team_registration.rb
@@ -3,5 +3,5 @@ class TeamRegistration < ActiveRecord::Base
   attr_accessible :current_points, :current_victory_points, :deleted_at, :name, :city, :paid_at, :payment_send, :penalty_points, :played_games, :roster_send, :roster_valid, :tournament_id
 
   belongs_to :tournament  
-
+  has_many :team_registration_players
 end
---
+++ test/unit/team_registration_player_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TeamRegistrationPlayerTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
